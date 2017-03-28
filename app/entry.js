c85ce240c29d44e8441315d1c23f9774fe42783a
Nikko Pisciotti <nikko@bluelacesmusic.com>
2017-03-28 15:25:49 -0700

    added entry.js

---
+++ app/entry.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const angular = require('angular');
+const camelcase = require('camelcase');
+const pascalcase = require('pascalcase');
+const path = require('path');
+const uiRouter = require('angular-ui-router');
+const ngTouch = require('angular-touch');
+const ngAnimate = require('angular-animate');
+
+const cfgram = angular.module('cfgram', [uiRouter, ngAnimate, ngTouch]);
+
+let context = require.context('./config/', true, /\.js$/);
+context.keys().forEach( key => {
+  cfgram.config(context(key));
+});
+
+context = require.context('./component/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  cfgram.component(name, module);
+});
+
+context = require.context('./service/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = camelcase(path.basename(key, '.js'));
+  let module = context(key);
+  cfgram.service(name, module);
+});
+
+context = require.context('./view/', true, /\.js$/);
+context.keys().forEach( key => {
+  let name = pascalcase(path.basename(key, '.js'));
+  let module = context(key);
+  cfgram.controller(name, module);
+});
